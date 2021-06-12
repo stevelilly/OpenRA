7c444dbf03af7acc3d4db85ea51a92de3e22d834
Steve Lilly <steve@jinjasoft.com>
2021-06-12 15:19:58 +0100

    wip implementation of Graham Scan for convexhulls

---
+++ OpenRA.Mods.Common/Traits/BotModules/ConvexHull.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenRA.Mods.Common.Traits.BotModules
+{
+	public class GrahamScan
+	{
+		public static CPos[] ConvexHull(CPos[] points)
+		{
+			// Find the point P0 with the lowest Y, then by lowest X
+			int p0x = points[0].X, p0y = points[0].Y;
+
+			foreach (CPos point in points)
+			{
+				if (point.Y < p0y || (point.Y == p0y && point.X < p0x))
+				{
+					p0x = point.X;
+					p0y = point.Y;
+				}
+			}
+
+			// Sort points by polar angle with P0
+			Array.Sort(points, new CompareAngleFromPoint(new CPos(p0x, p0y)));
+
+			CPos[] stack = new CPos[points.Length];
+			int topOfStack = -1;
+			foreach (CPos point in points)
+			{
+				while (topOfStack > 0 && Ccw(stack[topOfStack - 1], stack[topOfStack], point) <= 0)
+					topOfStack--;
+				stack[++topOfStack] = point;
+			}
+
+			Array.Resize(ref stack, topOfStack);
+			return stack;
+		}
+
+		private static int Ccw(CPos a, CPos b, CPos c)
+		{
+			return (b.X - a.X) * (c.Y - a.Y) - (b.Y - a.Y) * (c.X - a.X);
+		}
+	}
+
+	public class CompareAngleFromPoint : Comparer<CPos>
+	{
+		private readonly CPos o;
+
+		public CompareAngleFromPoint(CPos origin)
+		{
+			o = origin;
+		}
+
+		public override int Compare(CPos a, CPos b)
+		{
+			CVec av = a - o;
+			CVec bv = b - o;
+			int sa = Math.Sign(av.X);
+			int sb = Math.Sign(bv.X);
+			if (sa != sb)
+				return Math.Sign(bv.X - av.X);
+			return sa * Math.Sign(bv.X * av.Y - av.X * bv.Y);
+		}
+	}
+}
---
+++ OpenRA.Test/OpenRA.Mods.Common/ConvexHullTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using OpenRA.Mods.Common.Traits.BotModules;
+
+namespace OpenRA.Test.OpenRA.Mods.Common.Traits.BotModules
+{
+	[TestFixture]
+	class ConvexHullTest
+	{
+		[TestCase(TestName = "Convex hull of the unit square should output in clockwise order.")]
+		public void UnitSquare()
+		{
+			var points = new CPos[4];
+			points[0] = new CPos(1, 0);
+			points[1] = new CPos(0, 1);
+			points[2] = new CPos(1, 1);
+			points[3] = new CPos(0, 0);
+			var result = GrahamScan.ConvexHull(points);
+			if (!result.SequenceEqual(new[]
+			{
+				new CPos(1, 0),
+				new CPos(1, 1),
+				new CPos(0, 1),
+				new CPos(0, 0)
+			}))
+			{
+				Assert.Fail("points did not match, got " + string.Join(", ", result));
+			}
+		}
+
+		[TestCase(TestName = "Convex hull of a simple triangle should reject a point within it.")]
+		public void TriangleAndPoint()
+		{
+			var points = new CPos[4];
+			points[0] = new CPos(0, 0);
+			points[1] = new CPos(0, 3);
+			points[2] = new CPos(3, 0);
+			points[3] = new CPos(1, 1);
+			var result = GrahamScan.ConvexHull(points);
+			if (!result.SequenceEqual(new[]
+			{
+					new CPos(0, 4),
+					new CPos(0, 0),
+					new CPos(4, 0)
+			}))
+			{
+				Assert.Fail("points did not match, got " + string.Join(", ", result));
+			}
+		}
+
+		[TestCase(TestName = "SortFunc1")]
+		public void SortFunc1()
+		{
+			var compare = new CompareAngleFromPoint(new CPos(0, 0));
+
+			int result = compare.Compare(new CPos(3, 0), new CPos(1, 1));
+			Assert.AreEqual(-1, result);
+		}
+
+		[TestCase(TestName = "SortFunc2")]
+		public void SortFunc2()
+		{
+			var compare = new CompareAngleFromPoint(new CPos(0, 0));
+
+			int result = compare.Compare(new CPos(3, 0), new CPos(0, 3));
+			Assert.AreEqual(-1, result);
+		}
+
+		[TestCase(TestName = "SortFunc3")]
+		public void SortFunc3()
+		{
+			var compare = new CompareAngleFromPoint(new CPos(0, 0));
+
+			int result = compare.Compare(new CPos(1, 1), new CPos(0, 3));
+			Assert.AreEqual(-1, result);
+		}
+	}
+}
