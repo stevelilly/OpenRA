285dfebef13d6a7554c5c08bb29ceba46cdb347e
Steve Lilly <steve@jinjasoft.com>
2022-05-08 16:50:32 +0100

    Introduce DistanceMap

---
+++ OpenRA.Test/OpenRA.Mods.Common/DistanceMapTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+using OpenRA.Mods.Common.Traits.BotModules;
+
+namespace OpenRA.Test.OpenRA.Mods.Common.Traits.BotModules
+{
+	[TestFixture]
+	class DistanceMapTest
+	{
+		[TestCase(TestName = "Filled from a singularity")]
+		public void TestSingularity()
+		{
+			DistanceMap map = new DistanceMap(100, 100);
+			map.SetRoot(49, 49);
+			map.Solve();
+
+			Assert.Fail("got here");
+		}
+	}
+}
